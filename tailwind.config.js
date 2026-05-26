53eb30c93c481700127ad6665f83e9e441ca290b
Arvind Kumar Nalli Kuppuswami <nkarvindkumar@gmail.com>
2026-05-25 23:23:01 -0700

    style: refactor site-wide typography to Google Fonts style (Open Sans and Outfit)

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Open Sans"', 'Roboto', 'sans-serif'],
+        display: ['Outfit', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
